36ce23ebce22de9f2dc253bc74f4eaad284ab72a
Tyler Calder <calder-ty@protonmail.com>
2023-04-04 15:31:48 -0600

    feat: Add VolumeMeter Widget

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,4 @@
 //! UI Code for TMIX
-
 use std::{
     io::{self, Result},
     thread,
@@ -78,11 +77,15 @@ impl App {
 /// Module for defining UI stuff
 mod ui {
 
+
+    use log::{debug, error, info, warn};
     use tui::{
         backend::Backend,
-        layout::{Constraint, Direction, Layout},
+        buffer::Buffer,
+        layout::{Constraint, Direction, Layout, Rect},
         style::{Color, Style},
-        widgets::{Block, Borders, BarChart},
+        symbols,
+        widgets::{BarChart, Block, Borders, Widget},
         Frame,
     };
 
@@ -102,16 +105,93 @@ mod ui {
             )
             .split(f.size());
         for (i, chunk) in chunks.into_iter().enumerate() {
-            let data = &[("volume", 20 * i as u64)];
             let block = Block::default()
                 .title(format!("Window #{i}"))
                 .borders(Borders::ALL);
-            let bar = BarChart::default()
-                .block(block)
-                .bar_style(Style::default().fg(Color::DarkGray))
-                .data(data)
-                .max(100);
+            let bar = VolumeMeter::default().block(block).value(10u8.saturating_mul(i as u8) );
             f.render_widget(bar, chunk);
+        }
+    }
+
+    #[derive(Debug, Default)]
+    struct VolumeMeter<'a> {
+        value: u8,
+        block: Option<Block<'a>>,
+    }
+
+    impl<'a> VolumeMeter<'a> {
+        /// Set the block to embed the VolumeMeter in
+        pub fn block(mut self, b: Block<'a>) -> Self {
+            self.block = Some(b);
+            self
+        }
+
+        /// Set the Value of the Meter, Values > 100 will be set to 100
+        pub fn value(mut self, val: u8) -> Self {
+            self.value = val;
+            self
+        }
+    }
+
+    impl<'a> Widget for VolumeMeter<'a> {
+        fn render(mut self, area: Rect, buf: &mut Buffer) {
+            buf.set_style(area, Style::default().fg(Color::DarkGray));
+
+            // Get the Meter Area
+            let meter_area = match self.block.take() {
+                Some(b) => {
+                    let inner_area = b.inner(area);
+                    b.render(area, buf);
+                    inner_area
+                }
+                None => area,
+            };
+
+            // Not Enough space to draw
+            if meter_area.height < 2 {
+                return;
+            }
+
+            let max = 100;
+            let min = 0;
+
+            // Get the center of the cell
+            let center = meter_area.left() + meter_area.width / 2;
+            let top = meter_area.top() + 1;
+            let bottom = meter_area.height - 1;
+            let value_pos = bottom - ((bottom - top) * self.value as u16 / 100);
+
+
+            // Draw the Meter
+            for vert in top..=bottom {
+                let mut fg_color = Color::DarkGray;
+                let symbol = if vert == value_pos {
+                    fg_color = Color::Gray;
+                    buf.get_mut(center - 1, vert).set_symbol(symbols::bar::FULL).set_fg(fg_color);
+                    buf.get_mut(center + 1, vert).set_symbol(symbols::bar::FULL).set_fg(fg_color);
+                    symbols::bar::FULL
+                } else if vert == top {
+                    symbols::line::THICK.horizontal_down
+                } else if vert == bottom {
+                    symbols::line::THICK.horizontal_up
+                }
+                // TODO: Rather than do this modulo style, maybe we have it split in half until the
+                // space between eahc split is < 5. Then use that as the distance spread.
+                else if vert % (5) == 0 {
+                    symbols::line::THICK.cross
+                } else {
+                    symbols::line::THICK.vertical
+                };
+                buf.get_mut(center, vert).set_symbol(symbol).set_fg(fg_color);
+            }
+
+            buf.set_stringn(
+                meter_area.left() + 1,
+                meter_area.bottom() - 1,
+                format!("{}%", self.value),
+                5,
+                Style::default(),
+            );
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::io::Result;
 use app::App;
 
 fn main() -> Result<()> {
+    simple_logger::SimpleLogger::new().env().init().unwrap();
     // Setup the Terminal (Per Docs)
     let mut applicaton = App::default();
     applicaton.run()?;
