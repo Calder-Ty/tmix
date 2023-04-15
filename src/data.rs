a885b9933cbec4836bfc0bc9c0124e4741c3a91d
Tyler Calder <calder-ty@protonmail.com>
2023-04-15 06:46:37 -0600

    feat: Add Sink information

--- src/data.rs
+++ src/data.rs
@@ -1,12 +1,12 @@
 //! Data Structures for Pulse Audio
 use pulse::{
-    context::introspect::SinkInputInfo,
     channelmap,
-    format,
+    context::introspect::{SinkInfo, SinkInputInfo},
+    def, format,
     proplist::Proplist,
     sample,
     time::MicroSeconds,
-    volume::ChannelVolumes,
+    volume::{ChannelVolumes, Volume},
 };
 
 pub struct SinkInputInformation {
@@ -53,7 +53,6 @@ pub struct SinkInputInformation {
     pub format: format::Info,
 }
 
-
 impl From<&SinkInputInfo<'_>> for SinkInputInformation {
     fn from(value: &SinkInputInfo<'_>) -> Self {
         let name = value.name.as_ref().map(|x| x.to_string());
@@ -78,6 +77,82 @@ impl From<&SinkInputInfo<'_>> for SinkInputInformation {
             has_volume: value.has_volume.clone(),
             volume_writable: value.volume_writable.clone(),
             format: value.format.clone(),
+        }
+    }
+}
+
+pub struct SinkInformation {
+    /// Name of the sink.
+    pub name: Option<String>,
+    /// Index of the sink.
+    pub index: u32,
+    /// Description of this sink.
+    pub description: Option<String>,
+    /// Sample spec of this sink.
+    pub sample_spec: sample::Spec,
+    /// Channel map.
+    pub channel_map: channelmap::Map,
+    /// Index of the owning module of this sink, or `None` if is invalid.
+    pub owner_module: Option<u32>,
+    /// Volume of the sink.
+    pub volume: ChannelVolumes,
+    /// Mute switch of the sink.
+    pub mute: bool,
+    /// Index of the monitor source connected to this sink.
+    pub monitor_source: u32,
+    /// The name of the monitor source.
+    pub monitor_source_name: Option<String>,
+    /// Length of queued audio in the output buffer.
+    pub latency: MicroSeconds,
+    /// Driver name.
+    pub driver: Option<String>,
+    /// Flags.
+    pub flags: def::SinkFlagSet,
+    /// Property list.
+    pub proplist: Proplist,
+    /// The latency this device has been configured to.
+    pub configured_latency: MicroSeconds,
+    /// Some kind of “base” volume that refers to unamplified/unattenuated volume in the context of
+    /// the output device.
+    pub base_volume: Volume,
+    /// State.
+    pub state: def::SinkState,
+    /// Number of volume steps for sinks which do not support arbitrary volumes.
+    pub n_volume_steps: u32,
+    /// Card index, or `None` if invalid.
+    pub card: Option<u32>,
+    /// Set of formats supported by the sink.
+    pub formats: Vec<format::Info>,
+}
+
+impl<'a> From<&SinkInfo<'a>> for SinkInformation {
+    fn from(value: &SinkInfo<'a>) -> Self {
+        let name = value.name.as_ref().map(|x| x.to_string());
+        let driver = value.driver.as_ref().map(|x| x.to_string());
+        let description = value.description.as_ref().map(|x| x.to_string());
+        let monitor_source_name = value.monitor_source_name.as_ref().map(|x| x.to_string());
+
+        Self {
+            name,
+            index: value.index.clone(),
+            description,
+            sample_spec: value.sample_spec.clone(),
+            channel_map: value.channel_map.clone(),
+            owner_module: value.owner_module.clone(),
+            volume: value.volume.clone(),
+            mute: value.mute.clone(),
+            monitor_source: value.monitor_source.clone(),
+            monitor_source_name,
+            latency: value.latency.clone(),
+            driver,
+            flags: value.flags.clone(),
+            proplist: value.proplist.clone(),
+            configured_latency: value.configured_latency.clone(),
+            base_volume: value.base_volume.clone(),
+            state: value.state.clone(),
+            n_volume_steps: value.n_volume_steps.clone(),
+            card: value.card.clone(),
+            formats: value.formats.clone(),
         }
     }
 }
